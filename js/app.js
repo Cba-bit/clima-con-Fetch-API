19ac4650b2b40e0b6d04d237888288d19c755d37
cbabit <ignaciotagle26@gmail.com>
2020-10-21 10:17:02 -0300

    Validando formulario, mostrando alerta de error, y eliminar alerta despues de 5 segundos

--- js/app.js
+++ js/app.js
@@ -8,9 +8,48 @@ window.addEventListener('load', () => {
 
 function buscarClima(e) {
   e.preventDefault();
-  console.log('Buscando Clima...');
 
   // Validar
+  const ciudad = document.querySelector('#ciudad').value;
+  const pais = document.querySelector('#pais').value;
+
+  if (ciudad === '' || pais === '') {
+    // Hubo un error
+    mostrarError('Ambos campos son obligatorios');
+
+    return;
+  }
 
   // Consultar la api
+}
+
+function mostrarError(mensaje) {
+  const alerta = document.querySelector('.bg-red-100');
+  //IF Para que no se repita la alerta
+  if (!alerta) {
+    const alerta = document.createElement('div');
+    alerta.classList.add(
+      'bg-red-100',
+      'border-red-400',
+      'text-red-700',
+      'px-4',
+      'py-3',
+      'rounded',
+      'max-w-md',
+      'mx-auto',
+      'mt-6',
+      'text-center'
+    );
+    alerta.innerHTML = `
+      <strong class="font-bold">Error!</strong>
+      <span class="block">${mensaje}</span>
+      `;
+
+    container.appendChild(alerta);
+
+    // SetTimeOut para que se elimine la alerta
+    setTimeout(() => {
+      alerta.remove();
+    }, 5000);
+  }
 }
